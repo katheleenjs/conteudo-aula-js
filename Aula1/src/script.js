be1893efac54a6837eb9f8fb3adbe83d23dc2165
katheleen <katheleenjds@gmail.com>
2026-02-24 22:11:30 -0300

    Aula 1 - Variáveis em JavaScript

---
+++ Aula1/src/script.js
@@ -0,0 +1,11 @@
+//Exemplos de variável let
+let nome = 'Katheleen';
+let nome2 = 'Jesus';
+
+console.log(nome);
+console.log(nome2);
+
+// Exemplos de variável const
+const times = ['Corinthians', 'São Paulo', 'Vasco'];
+
+console.log(times);
