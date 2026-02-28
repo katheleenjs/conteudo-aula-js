77a34df503c685eb61d7542162dcbcbc3d5c7bf2
katheleen <katheleenjds@gmail.com>
2026-02-27 22:12:23 -0300

    Aula 4 - Condições Lógicas (Else, Switch e Ternário)

---
+++ Aula4/src/script02.js
@@ -0,0 +1,33 @@
+// Exemplo de If Else
+// const hora = 18;
+// const minutos = new Date().getMinutes();
+// const horario = String(hora) + ':' + String(minutos);
+// let saudacao;
+
+// if (hora > 18) {
+//     saudacao = 'Boa noite!';
+// } else {
+//     saudacao = 'Bom dia!';
+// }
+
+// document.getElementById('ex1').innerHTML = saudacao;
+// // document.getElementById('ex2').innerHTML = 'Agora são: ' + horario;
+// document.getElementById('ex2').innerHTML = `Agora são: ${horario}`;
+
+// Exemplo If Else If
+const hora = 7;
+const minutos = new Date().getMinutes();
+const horario = String(hora) + ':' + String(minutos);
+let saudacao;
+
+if (hora > 18) {
+    saudacao = 'Boa noite!';
+} else if (hora > 12) {
+    saudacao = 'Boa tarde!';
+} else {
+    saudacao = 'Bom dia!';
+}
+
+document.getElementById('ex1').innerHTML = saudacao;
+// document.getElementById('ex2').innerHTML = 'Agora são: ' + horario;
+document.getElementById('ex2').innerHTML = `Agora são: ${horario}`;
---
+++ Aula4/src/script03.js
@@ -0,0 +1,5 @@
+// Exemplo de condição ternária
+let idade = 18;
+let texto = (idade == 19) ? 'true!' : 'false!';
+
+document.getElementById('ex1').innerHTML = texto;
