f275e99df4ba0f802817229e2ded4a3e461dbc35
katheleen <katheleenjds@gmail.com>
2026-03-04 22:13:53 -0300

    Aula 6 - Exercícios de Lógica de Programação em JS

--- Aula6/src/exercicio10.js
+++ Aula6/src/exercicio10.js
@@ -1,4 +1,4 @@
-// Exercício 10: Faça um algoritmo que leia a idade de uma pessoa expressa em anos, meses e dias e escreva a idade dessa pessoa expressa apenas em dias. Considerar ano com 365 dias e mês com 30 dias.
+// Exercício 10: Faça um algoritmo que leia a idade de uma pessoa expressa em anos, meses e dias, e escreva a idade dessa pessoa expressa apenas em dias. Considerar ano com 365 dias e mês com 30 dias.
 
 alert("Idade em Dias");
 
