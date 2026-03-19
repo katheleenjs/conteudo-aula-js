ef19f241ca24b7513022ffff7b0c5acd18912bc2
katheleen <katheleenjds@gmail.com>
2026-03-19 19:32:32 -0300

    Aula 16 - Finalizando alterações em HTML

---
+++ Aula16/src/script.js
@@ -0,0 +1,11 @@
+// Alterando atributo
+const elemento = document.querySelector(".ex1");
+
+elemento.src = "Frame2.jpg";
+elemento.with = "400"
+
+// Conteúdo dinâmico (data)
+const elemento2 = document.querySelector("#ex2");
+
+elemento2.innerHTML = "Data atual: " + Date();
+
