ee892c8ff5650906012b3d228f5495f07f52a386
katheleen <katheleenjds@gmail.com>
2026-03-18 22:09:45 -0300

    Aula 15 - Estudando Manipulações de Elementos do HTML

---
+++ Aula15/src/script03.js
@@ -0,0 +1,8 @@
+// Utilizando o innerHTML
+const elemento = document.querySelector(".mudandoElemento");
+
+elemento1.innerHTML = "Somente a dor coletiva gera união.";
+
+// Alterando atributos
+const elemento2 = document.querySelector(".foto");
+elemento.src = "Frame1.png";
